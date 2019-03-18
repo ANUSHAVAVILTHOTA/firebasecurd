db56f60e07585d6c051f259720f9674fb2ebaab4
ANUSHAVAVILTHOTA <anusha.v1999@gmail.com>
2019-03-18 18:18:13 +0530

    Firstcommit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDw5-MrF0-vx6QwiNaMmlzVuj-jfc42Edc",
+    authDomain: "fir-curd-f414e.firebaseapp.com",
+    databaseURL: "https://fir-curd-f414e.firebaseio.com",
+    projectId: "fir-curd-f414e",
+    storageBucket: "fir-curd-f414e.appspot.com",
+    messagingSenderId: "970878125316"
+  }
 };
